990a3e6d445b97fa60802f94c5478c313151fa3e
mnxx2 <alsdk4931@gmail.com>
2025-09-22 05:43:23 +0900

    models/qna 수정, package.json test의 exist 0 수정

--- src/models/qna.js
+++ src/models/qna.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const { ENUM, Association } = require("sequelize");
-=======
-const { ENUM } = require("sequelize");
->>>>>>> c590c19 ([250728]1. post,qna 테이블 생성 및 post CRUD 추가)
 
 module.exports = (sequelize, DataTypes) => {
   const Qna = sequelize.define(
@@ -16,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-<<<<<<< HEAD
       answer: {
         type: DataTypes.TEXT,
         allowNull: true,
@@ -31,18 +26,6 @@ module.exports = (sequelize, DataTypes) => {
           "기타"
         ),
         defaultValue: "로그인",
-=======
-      qna_type: {
-        type: DataTypes.ENUM(
-          "login",
-          "ledger",
-          "analysis",
-          "error",
-          "user",
-          "etc"
-        ),
-        defaultValue: "login",
->>>>>>> c590c19 ([250728]1. post,qna 테이블 생성 및 post CRUD 추가)
         allowNull: false,
       },
       iscompleted: {
@@ -53,14 +36,11 @@ module.exports = (sequelize, DataTypes) => {
     },
     { tableName: "qna" }
   );
-<<<<<<< HEAD
   Qna.associate = function (models) {
     Qna.belongsTo(models.User, {
       foreignKey: "userId",
       as: "user",
     });
   };
-=======
->>>>>>> c590c19 ([250728]1. post,qna 테이블 생성 및 post CRUD 추가)
   return Qna;
 };
